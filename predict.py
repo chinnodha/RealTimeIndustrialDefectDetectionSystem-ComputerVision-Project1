2d97ab39bb64c486ce43806598c451d8f2a66bc6
NIKHILAKS-hub <nikhilas2009@gmail.com>
2026-07-15 15:56:29 +0300

    training complete and added prediction

---
+++ predict.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Load the trained model
+model = YOLO("C:\\Users\\ADMIN\\Desktop\\runs\\detect\\runs\\train\\defect_detection\\weights\\best.pt")
+
+# Predict on an image
+results = model.predict(
+    source="test_images/sample.jpg",
+    conf=0.2,
+    save=True,
+    show=True
+)
+
+print("Prediction completed!")
